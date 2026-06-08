2858c266ecab3f4c22bf940e0659311b9124fc5a
benC123321 <benlcollier@gmail.com>
2026-06-08 17:44:53 -0500

    Controlling single motor with python

--- src/python/main.py
+++ src/python/main.py
@@ -1,6 +1,8 @@
 from uart_handler import UARTHandler
 from checksum import lobot_check_sum
 
+
+# This interface must be enabled in board config prior to use
 uart = UARTHandler(
     port="/dev/ttyAMA0",
     baudrate=115200
@@ -11,7 +13,6 @@ message = [
     0x55,
     0x01,
     0x07,
-    0x03,
     0x01,
     0xF4,
     0x01,
